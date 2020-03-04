8c356c86e70cc453aad6721d554b0acd93fcc85b
João Vitor <jv17fc@gmail.com>
2020-03-04 19:51:23 -0300

    criando paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'botoes',
+    loadChildren: () => import('./botoes/botoes.module').then( m => m.BotoesPageModule)
+  },
+  {
+    path: 'cards',
+    loadChildren: () => import('./cards/cards.module').then( m => m.CardsPageModule)
+  },
+  {
+    path: 'listas',
+    loadChildren: () => import('./listas/listas.module').then( m => m.ListasPageModule)
+  },
 ];
 
 @NgModule({
